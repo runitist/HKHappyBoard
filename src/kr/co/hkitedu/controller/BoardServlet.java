7f962139588b6df30d22a57db49ee903d37cfc40
USER1-PC\USER <runitist@gmail.com>
2018-10-16 12:13:51 +0900

    보드 서블릿 날림

--- src/kr/co/hkitedu/controller/BoardServlet.java
+++ src/kr/co/hkitedu/controller/BoardServlet.java
@@ -27,33 +27,13 @@ public class BoardServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		TempSectionPageStat tsps = new TempSectionPageStat();
-		String wb = request.getParameter("wb");
+		String wb = request.getParameter("wb");//무슨 보드인가?
 		String wbq = "";// 쿼리문으로 넘겨줄 보드명
 		String wbp = "1";// 보드 페이지. 기본값은 1.
 		final int pgmax = 20;//페이지 최대 로우개수
-
-		tsps.setWps("Board");
-		if (wb.equals("1")) {
-			tsps.setWpsn("유머 게시판");
-			wbq = "h_board1";
-		} else if (wb.equals("2")) {
-			tsps.setWpsn("학습 게시판");
-			wbq = "h_board2";
-		} else if (wb.equals("3")) {
-			tsps.setWpsn("자유 게시판");
-			wbq = "h_board3";
-		}
-
-		if (request.getParameter("wbp") != null) {
-			wbp = request.getParameter("wbp");
-		}
-
-		ArrayList<BoardVO> bvl = DAO_OracleQuery.getBvl(wbq);
-		// dao로 넘겨줘야할 자료. wbq 페이지
-		
-
-		int ctr = DAO_OracleQuery.getCountAllRows(wbq);//해당 보드 전체 로우수
-		int pgn = (ctr/20)+1;//페이지 
+		ArrayList<BoardVO> bvl = null;
+		int ctr = 0;//해당 보드 전체 로우수
+		int pgn = 0;
 		
 		request.setAttribute("bvl", bvl);
 		request.setAttribute("tsps", tsps);
---
+++ src/kr/co/hkitedu/controller/tststs.java
@@ -0,0 +1,13 @@
+package kr.co.hkitedu.controller;
+
+import kr.co.hkitedu.model.DAO.DAO_OracleQuery;
+
+public class tststs {
+	//dao 쿼리문을 시험하는 클래스
+
+	public static void main(String[] args) {
+		System.out.println(DAO_OracleQuery.getCountAllRows("h_board1"));
+
+	}
+
+}
